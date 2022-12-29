f76f1bff30cb5d95adafede424708148ada46c5d
xauravww <sauravmaheshwari8@gmail.com>
2022-12-29 22:55:53 +0530

    First day work

---
+++ crackbook_frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Routes, Route, useNavigate } from "react-router-dom"
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
+  )
+}
+
+export default App
